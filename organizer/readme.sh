3ae7d7d2863f6eaec65478fce8f4cc73c9287364
Sunny Ravindra <sunnyravbusiness@gmail.com>
2023-06-17 22:34:11 +0530

    Minor Fix: 2023-06-17 22:34:11

--- organizer/readme.sh
+++ organizer/readme.sh
@@ -21,47 +21,55 @@ while IFS= read -r line; do
     fi
 
 done < "$file"
+pwd
+# Iterate through each file in the current directory
+# Iterate through each file in the current directory
 
 string=$(echo -e $output | tr -d '#')
-# IFS=$'\n' read -r -d '' -a lines <<< "$string"
+IFS=$'\n' read -r -d '' -a lines <<< "$string"
 
-# # Iterate over each line and remove the space after '['
-# new_lines=()
+pwd
+# Iterate over each line and remove the space after '['
+new_lines=()
 
-# for line in "${lines[@]}"; do
-#     index=$(echo "$line" | awk '{print index($0, "[")}')
-#     if (( index > 0 && index < ${#line} - 1 )); then
-#         line="${line:0:index}${line:index+1}"
-#     fi
-#     new_lines+=("$line")
-# done
+for line in "${lines[@]}"; do
+    index=$(echo "$line" | awk '{print index($0, "[")}')
+    if (( index > 0 && index < ${#line} - 1 )); then
+        line="${line:0:index}${line:index+1}"
+    fi
+    new_lines+=("$line")
+done
+pwd
 
-# # Join the modified lines back into a single string
-# new_string=$(IFS=$'\n'; echo "${new_lines[*]}")
+# Join the modified lines back into a single string
+new_string=$(IFS=$'\n'; echo "${new_lines[*]}")
 
-# # Split the string into lines
-# IFS=$'\n' read -r -d '' -a lines <<< "$new_string"
+# Split the string into lines
+IFS=$'\n' read -r -d '' -a lines <<< "$new_string"
+pwd
 
-# # Iterate over each line and replace "( " with "(#"
-# new_lines=()
-# for line in "${lines[@]}"; do
-#     new_line=${line//"( "/"(#"}
-#     new_lines+=("$new_line")
-# done
+# Iterate over each line and replace "( " with "(#"
+new_lines=()
+for line in "${lines[@]}"; do
+    new_line=${line//"( "/"(#"}
+    new_lines+=("$new_line")
+done
+pwd
 
-# new_string=$(IFS=$'\n'; echo "${new_lines[*]}")
-#     fi
-# done
-# # Print the updated string
-# echo "$new_string" > ./2-index.md
+new_string=$(IFS=$'\n'; echo "${new_lines[*]}")
+    fi
+done
+pwd
+# Print the updated string
+echo "$new_string" > ./2-index.md
+pwd
 
-# cat ./1-Project-name.md > ../README.md
-# cat ./2-index.md >> ../README.md
+cat ./1-Project-name.md > ../README.md
+cat ./2-index.md >> ../README.md
 
-# for file in *; do
-#     if [[ -f "$file" && "$file" != "1-Project-name.md" && "$file" != "2-index.md" && "$file" != "readme.sh" ]]; then
-#         cat $file >> ../README.md
-#     fi
-# done
-
-# sed -i 's/ - \[/  - \[/' ../README.md
+for file in *; do
+    if [[ -f "$file" && "$file" != "1-Project-name.md" && "$file" != "2-index.md" && "$file" != "readme.sh" ]]; then
+        cat $file >> ../README.md
+    fi
+done
+sed -i 's/ - \[/  - \[/' ../README.md
