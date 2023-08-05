4f6df09c20ef59d3d122c79e54af069b253ca08d
sunnyRavindra <sunnyravbharne@gmail.com>
2023-08-05 09:57:03 +0530

    added White Navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "633ded46cca80872"; }
+/******/ 		__webpack_require__.h = function() { return "b5c5af461c83126d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,35 +4,35 @@ import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-center h-16 border border-zinc-800">
+    <div className="flex items-center justify-center h-16 border border-l-stone-500">
       <div className="flex row-flex justify-between basis-3/4 w-28">
-        <div className="flex row-flex justify-between bg-black w-1/3">
-         <div className="flex items-center justify-center text-center text-sm text-stone-400 ">
-             <Image src="/botcat.png" alt="Botcat Logo" className="dark:invert" width={100} height={24} priority />
-         </div>
-         <div className="flex items-center justify-center text-center text-sm text-stone-400">
-             <Link href="/posts/first-post">Profile</Link>
-         </div>
-         <div className="flex items-center justify-center text-center text-sm text-stone-400">
-             <Link href="/posts/first-post">Resume</Link>
-         </div>
-         <div className="flex items-center justify-center text-center text-sm text-stone-400">
-             <Link href="/posts/first-post">Projects</Link>
-         </div>
-         <div className="flex items-center justify-center text-center text-sm text-stone-400">
-             <Link href="/posts/first-post">Contact me</Link>
-         </div>
-         <div className="flex items-center justify-center text-center text-sm text-stone-400">
-             <Link href="/posts/first-post">About</Link>
-         </div>
+        <div className="flex row-flex justify-between w-1/3">
+            <div className="flex items-center justify-center text-center text-sm text-stone-400 ">
+                <Image src="/botcat.png" alt="Botcat Logo" className="dark:invert" width={100} height={24} priority />
+            </div>
+            <div className="flex items-center justify-center text-center text-sm text-neutral-500 mx-4">
+                <Link href="/posts/first-post">Profile</Link>
+            </div>
+            <div className="flex items-center justify-center text-center text-sm text-neutral-500 mx-4">
+                <Link href="/posts/first-post">Resume</Link>
+            </div>
+            <div className="flex items-center justify-center text-center text-sm text-neutral-500 mx-4">
+                <Link href="/posts/first-post">Projects</Link>
+            </div>
+            <div className="flex items-center justify-center text-center text-sm text-neutral-500 mx-4">
+                <Link href="/posts/first-post">About</Link>
+            </div>
         </div>
-        <div className="flex row-flex flex-row-reverse bg-black w-1/2" >
-         <div className="flex items-center justify-center bg-white h-8 w-20 text-center rounded-full text-neutral-950 rounded-md mx-1 text-sm font-medium">
-             <Link href="/posts/first-post">Sign Up</Link>
-         </div>
-         <div className="flex items-center justify-center bg-neutral-950 h-8 w-16 text-center rounded-full text-gray-50 rounded-md  bg-zinc-900 mx-1 text-sm border border-neutral-700">
-             <Link href="/posts/first-post">Log In</Link>
-         </div>
+        <div className="flex row-flex flex-row-reverse w-1/2" >
+            <div className="flex items-center justify-center h-8 w-20 text-center rounded-full text-white rounded-md mx-1 text-sm font-medium bg-zinc-900">
+                <Link href="/posts/first-post">Sign Up</Link>
+            </div>
+            <div className="flex items-center justify-center bg-grey-950 h-8 w-16 text-black text-center rounded-full text-gray-50 rounded-md mx-1 text-sm border border-neutral-700">
+                <Link href="/posts/first-post">Log In</Link>
+            </div>
+            <div className="flex items-center justify-center text-center text-sm text-neutral-500 mx-4">
+                <Link href="/posts/first-post">Contact me</Link>
+            </div>
         </div>
       </div>
     </div>
