4eec0c1ec8ecaf6b1d01780146d93fa3f91256f2
Sunny Bharne <sunnybharne@Sunnys-Air.domain.name>
2023-08-15 10:10:21 +0530

    Removed unsed fles

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: '/resume', title: 'Resume' },
   { href: '/blog', title: 'My Blog' },
   { href: '/projects', title: 'My Projects' },
   { href: '/about', title: 'About Me' },
