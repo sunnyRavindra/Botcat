38fced1035719184fcf3a69c5b97436f8d004952
sunnyRavindra <sunnyravbharne@gmail.com>
2023-08-20 23:55:51 +0530

    added dacusorous doc site

--- data/headerNavLinks.ts
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/resume', title: 'Resume' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
-]
-
-export default headerNavLinks
